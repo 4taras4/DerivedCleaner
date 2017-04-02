75feeec8b964ca006c0676918d80c7ce432a83e0
4taras4 <4taras4@gmail.com>
2017-04-02 17:32:11 +0300

    Initial Commit

---
+++ StatusBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StatusBar
+//
+//  Created by Taras Markevych on 02.04.17.
+//  Copyright © 2017 Taras Markevych. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StatusBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StatusBar
+//
+//  Created by Taras Markevych on 02.04.17.
+//  Copyright © 2017 Taras Markevych. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
