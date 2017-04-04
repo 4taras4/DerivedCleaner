742af80d190d4bc10fed697232824666d3deac00
4taras4 <4taras4@gmail.com>
2017-04-04 21:47:08 +0300

    added all

---
+++ StatusBar/Commands.swift
@@ -0,0 +1,13 @@
+//
+//  Commands.swift
+//  StatusBar
+//
+//  Created by Taras Markevych on 02.04.17.
+//  Copyright © 2017 Taras Markevych. All rights reserved.
+//
+
+import Cocoa
+
+class Commands: NSObject {
+
+}
